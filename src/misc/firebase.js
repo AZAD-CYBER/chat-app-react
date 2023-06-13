4b2ac1ff50ac152e2ba159aac3f7b8b079cb6e9d
Azad <azadansari7504@gmail.com>
2023-06-13 17:27:16 +0530

    Signin

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,21 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/database';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_E2uX7vTgGhAmpK0TB-2wh0FdbJsqSU4",
-  authDomain: "chat-web-app-65d5f.firebaseapp.com",
-  projectId: "chat-web-app-65d5f",
-  storageBucket: "chat-web-app-65d5f.appspot.com",
-  messagingSenderId: "967210762543",
-  appId: "1:967210762543:web:91ab26eae8b73d5c2821da",
-  measurementId: "G-FZ0Y392E2Z"
+  apiKey: 'AIzaSyB_E2uX7vTgGhAmpK0TB-2wh0FdbJsqSU4',
+  authDomain: 'chat-web-app-65d5f.firebaseapp.com',
+  projectId: 'chat-web-app-65d5f',
+  storageBucket: 'chat-web-app-65d5f.appspot.com',
+  messagingSenderId: '967210762543',
+  appId: '1:967210762543:web:91ab26eae8b73d5c2821da',
+  measurementId: 'G-FZ0Y392E2Z',
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = firebase.initializeApp(firebaseConfig);
+const auth = app.auth();
+const database = app.database();
+
+export { auth, database };
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import firebase from 'firebase/compat/app';
+import { Container, Grid, Row, Panel, Col, Button } from 'rsuite';
+import { Icon } from '@rsuite/icons';
+import { auth, database } from '../misc/firebase';
 
-function SignIn() {
-  return <div>SignIn</div>;
-}
+const SignIn = () => {
+  // Function to sign in with a provider (e.g., Google or Facebook)
+  const signInWithProvider = async (provider) => {
+    try {
+      // Sign in with the specified provider using Firebase's signInWithPopup method
+      const { additionalUserInfo, user } = await auth.signInWithPopup(provider);
+
+      // Check if the user is signing in for the first time
+      if (additionalUserInfo.isNewUser) {
+        // If it's a new user, save their profile information in the database
+        await database.ref(`/profiles/${user.uid}`).set({
+          name: user.displayName,
+          createdAt: firebase.database.ServerValue.TIMESTAMP,
+        });
+      }
+
+      // Display a success alert indicating successful sign-in
+      window.alert('Signed in', 4000);
+    } catch (err) {
+      // Display an error alert if sign-in fails
+      window.alert(err.message, 4000);
+    }
+  };
+
+  // Function to handle Facebook sign-in
+  const onFacebookSignIn = () => {
+    signInWithProvider(new firebase.auth.FacebookAuthProvider());
+  };
+
+  // Function to handle Google sign-in
+  const onGoogleSignIn = () => {
+    signInWithProvider(new firebase.auth.GoogleAuthProvider());
+  };
+
+  return (
+    <Container>
+      <Grid className="mt-page">
+        <Row>
+          <Col xs={24} md={12} mdOffset={6}>
+            <Panel>
+              <div className="text-center">
+                <h2>Welcome to Chat</h2>
+                <p>Progressive chat platform for neophytes</p>
+              </div>
+
+              <div className="mt-3">
+                {/* Button to sign in with Facebook */}
+                <Button block color="blue" onClick={onFacebookSignIn}>
+                  <Icon icon="facebook" /> Continue with Facebook
+                </Button>
+
+                {/* Button to sign in with Google */}
+                <Button block color="green" onClick={onGoogleSignIn}>
+                  <Icon icon="google" /> Continue with Google
+                </Button>
+              </div>
+            </Panel>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
+  );
+};
 
 export default SignIn;
